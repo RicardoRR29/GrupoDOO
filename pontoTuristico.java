ef1bf66cca70ab2226843673a866d40bd73841e6
ThiagoCSgit <thiago.csdossantos@gmail.com>
2022-02-02 12:46:36 -0300

    singleton

--- pontoTuristico.java
+++ pontoTuristico.java
@@ -3,45 +3,45 @@ import Doo.singletonPontoTuristico;
 
 
 public class PontoTuristico {
-    // private String nome;
-    // private String complemento;
-    // private String descricao;
+    private String nome;
+    private String complemento;
+    private String descricao;
     
     public singletonPontoTuristico(){
         singletonPontoTuristico ponto = getInstance();
     }
     
-    // public void setPontoTuristico(PontoTuristico ponto){
-    //     this.nome = ponto.nome;
-    //     this.complemento = ponto.complemento;
-    //     this.descricao = ponto.descricao;
+    public void setPontoTuristico(PontoTuristico ponto){
+        this.nome = ponto.nome;
+        this.complemento = ponto.complemento;
+        this.descricao = ponto.descricao;
         
-    // }
+    }
     
 
 
-    // public String getNome(){
-    //     return nome;
-    // }
+    public String getNome(){
+        return nome;
+    }
 
-    // public void setNome(String nome){
-    //     this.nome = nome;
-    // }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
-    // public String getComplemento(){
-    //     return complemento;
-    // }
+    public String getComplemento(){
+        return complemento;
+    }
 
-    // public void setComplemento(String complemento){
-    //     this.complemento = complemento;
-    // }
+    public void setComplemento(String complemento){
+        this.complemento = complemento;
+    }
     
-    // public String getDescricao(){
-    //     return descricao;
-    // }
+    public String getDescricao(){
+        return descricao;
+    }
 
-    // public void setDescricao(String descricao){
-    //     this.descricao = descricao;
-    // }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
 
 }
